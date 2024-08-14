2f37115e19c7e2c05492f361dc40c768f4ec4167
appdevdqp <appdev.dqp@gmail.com>
2024-08-14 09:39:28 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,78 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/dan.jpg'),
+              ),
+              Text(
+                'Dan Dizon',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'Junior App Developer'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal.shade200,
+                  fontFamily: 'Source Sans 3',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+63 912 345 6789',
+                    style: TextStyle(
+                      color: Colors.teal.shade500,
+                      fontFamily: 'Source Sans 3',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'dan@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans 3',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade500,
+                      ),
+                    ),
+                  )
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
